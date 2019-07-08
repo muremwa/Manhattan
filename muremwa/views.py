06499364376924e13be3c13d66bc1b3f15a63ebc
Muremwa <danmburu254@gmail.com>
2019-07-08 11:25:08 +0300

    fixed a bug

--- muremwa/views.py
+++ muremwa/views.py
@@ -16,7 +16,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return redirect(reverse('blog:index'))
 
     else:
         form = SignUpForm()
